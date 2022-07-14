622599291a5310f42a6d004725dad25f4167999c
chrisphine <onyangoketta@gmail.com>
2022-07-14 10:57:35 +0300

    import .RMFunctions

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,8 @@
 import React from "react";
 import MesaVerde from "./parks/MesaVerde";
 import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 function ColoradoStateParks(){
-
   return (
     <div>
       <MesaVerde />
